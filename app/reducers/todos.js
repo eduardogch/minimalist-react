60f2f91666740b255976c91e713808136305bca5
Eduardo Gonzalez <eduardo_gonzalez@homedepot.com>
2018-05-11 11:01:05 -0400

    Add eslint and change dependencies

--- app/reducers/todos.js
+++ app/reducers/todos.js
@@ -2,7 +2,7 @@ const todos = (state = [], action) => {
     switch (action.type) {
         case "ADD_TODO":
             return [
-                ...state,
+                state,
                 {
                     id: action.id,
                     text: action.text,
@@ -13,7 +13,7 @@ const todos = (state = [], action) => {
             return state.map(
                 todo =>
                     todo.id === action.id
-                        ? { ...todo, completed: !todo.completed }
+                        ? { todo, completed: !todo.completed }
                         : todo
             );
         default:
